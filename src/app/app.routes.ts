b1b15ad358baeb67f6f977857554ed735d796a5c
RapuJeanette <rapupena2909@gmail.com>
2024-10-22 09:12:13 -0400

    2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
+import { DoctorComponent } from './doctor/doctor.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { RegistroComponent } from './registro/registro.component';
 
 export const routes: Routes = [
   { path: 'registro', component: RegistroComponent },  // Ruta para el registro
-  { path: '', redirectTo: '/registro', pathMatch: 'full' }, 
+  { path: 'doctor', component: DoctorComponent },
+  { path: 'admin', component: AdminPanelComponent }, 
+  { path: '', redirectTo: '/registro', pathMatch: 'full' },
 ];
---
+++ src/app/doctor/doctor.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-doctor',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './doctor.component.html',
+  styleUrl: './doctor.component.css'
+})
+export class DoctorComponent implements OnInit {
+  doctores: any[] = [];
+  mostrarModal = false;
+  nuevoDoctor = {
+    telefono: '',
+    especialidad: '',
+    disponibilidad: ''
+  };
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.obtenerDoctores();
+  }
+
+  obtenerDoctores() {
+    this.http.get('http://127.0.0.1:8000/doctores/')
+      .subscribe((response: any) => {
+        this.doctores = response;
+      });
+  }
+
+  agregarDoctor() {
+    this.http.post('http://127.0.0.1:8000/doctores/', this.nuevoDoctor)
+      .subscribe(() => {
+        this.obtenerDoctores();  // Actualizar la lista de doctores
+        this.nuevoDoctor = { telefono: '', especialidad: '', disponibilidad: '' };  // Limpiar el formulario
+        this.cerrarModal();
+      });
+  }
+
+  eliminarDoctor(id: string) {
+    this.http.delete(`http://127.0.0.1:8000/doctores/${id}`)
+      .subscribe(() => {
+        this.obtenerDoctores();  // Actualizar la lista de doctores
+      });
+  }
+
+  abrirModal() {
+    this.mostrarModal = true;
+  }
+
+  // Función para cerrar el modal
+  cerrarModal() {
+    this.mostrarModal = false;
+    this.nuevoDoctor = { telefono: '', especialidad: '', disponibilidad: '' };  // Limpiar el formulario
+  }
+}
